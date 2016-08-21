16ce0abae3544fea98bfe28d6b1e1e76d3851edb
davide <davide.faconti@gmail.com>
2016-08-21 19:22:08 +0200

    updated

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 
 using namespace std;
 
+typedef struct{
+  float x,y,theta;
+}Position2D;
+
 void bumpReceived()
 {
    cout << "Bump! From thread " << boost::this_thread::get_id() << endl;
@@ -10,7 +14,15 @@ void bumpReceived()
 
 void sensorReceived(float value)
 {
-  cout << "value " << value << " from thread " << boost::this_thread::get_id() << endl;
+  cout << "Sensor value " << value << " from thread " << boost::this_thread::get_id() << endl;
+}
+
+void positionReceived(const Position2D& pos)
+{
+  cout << "Robot position " <<  pos.x
+       << "/" << pos.y
+       << "/" << pos.theta
+       << " from thread " << boost::this_thread::get_id() << endl;
 }
 
 
@@ -22,24 +34,33 @@ int main(int argc, char *argv[])
 
   auto bump_emitter   = dispatcher.createEmitter<void()>("bump");
   auto sensor_emitter = dispatcher.createEmitter<void(float)>("sensor");
+  auto position_emitter = dispatcher.createEmitter<void(const Position2D&)>("position");
 
-  boost::function<void()> bump_funtor = boost::bind( &bumpReceived);
-  dispatcher.connect( std::string("bump") , bump_funtor  );
+  boost::function<void()> bump_functor = boost::bind( &bumpReceived);
+  dispatcher.connect( std::string("bump") , bump_functor  );
 
-  boost::function<void(float)> sensor_funtor = boost::bind( &sensorReceived, _1);
-  dispatcher.connect( std::string("sensor") , sensor_funtor );
+  boost::function<void(float)> sensor_functor = boost::bind( &sensorReceived, _1);
+  dispatcher.connect( std::string("sensor") , sensor_functor );
 
-  cout << "------ Synchronous -------" << endl;
+  boost::function<void(const Position2D&)> position_functor = boost::bind( &positionReceived, _1);
+  dispatcher.connect( std::string("position") , position_functor );
+
+  Position2D robot_position = {1,2,3};
+
+  cout << "\n------ Synchronous -------" << endl;
   bump_emitter->invokeSync();
   sensor_emitter->invokeSync(42);
+  position_emitter->invokeSync(robot_position);
 
-  cout << "------ Asynchronous -------" << endl;
+  cout << "\n------ Asynchronous -------" << endl;
   bump_emitter->invokeSync();
   sensor_emitter->invokeAsync(42);
+  position_emitter->invokeAsync(robot_position);
+
 
   boost::this_thread::sleep(boost::posix_time::milliseconds(100));
 
-  cout << "-------------" << endl;
+  cout << "\n-------------" << endl;
   cout << "Done from thread is " << boost::this_thread::get_id() << endl;
 
   return 0;
